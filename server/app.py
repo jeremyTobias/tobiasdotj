5f424f41f7edecffde8e5a7e8538275701035b42
Jeremy Tobias <jeremy.tobias@baesystems.com>
2021-08-06 21:09:17 -0400

    config updates...

--- server/app.py
+++ server/app.py
@@ -37,9 +37,5 @@ def rideshare():
         'fake': 'Rideshare Data'}]
     })
 
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
-
 if __name__ == '__main__':
     app.run(port=(os.getenv('PORT') if os.getenv('PORT') else 8080))
